a84f2b382310608545e033feeecf63e46caad6d9
dsmdongly <gdh0608@naver.com>
2018-05-09 10:24:57 +0900

    apply prodconfig

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,15 @@ from app.model import Mongo
 from app.view import Router
 
 from config.dev import DevConfig
+from config.prod import ProdConfig
+
 from flasgger import Swagger
 from flask import Flask
 
 
-def create_app():
+def create_app(dev=True):
     instance = Flask(__name__)
-    instance.config.from_object(DevConfig)
+    instance.config.from_object(DevConfig if dev else ProdConfig)
 
     Swagger(template=TEMPLATE).init_app(instance)
     Router().init_app(instance)
@@ -18,4 +20,4 @@ def create_app():
     return instance
 
 
-app = create_app()
+app = create_app(False)
